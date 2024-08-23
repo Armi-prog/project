f7d504eeda7f6ed85d334294a2fd8e689973e20f
Armi-prog <armi-a@yandex.ru>
2024-08-23 22:55:41 +0400

    movie DB

--- js/script.js
+++ js/script.js
@@ -14,25 +14,6 @@
 
 "use strict";
 
-const adv = document.querySelectorAll(".promo__adv img");
-const genre = document.querySelector(".promo__genre");
-const poster = document.querySelector(".promo__bg");
-const movieList = document.querySelector(".promo__interactive-item")
-
-adv.forEach(item => item.remove());
-genre.textContent = "Драма";
-poster.style.backgroundImage = "url(img/bg.jpg)";
-
-movieDB.movies.sort().forEach((movie, i) => {
-    movieList.innerHTML += `
-       <li class="promo__interactive-item">
-            ${i + 1}. 
-            ${movie}
-            <div class="delete"></div>
-        </li>
-        `;
-});
-
 const movieDB = {
     movies: [
         "Логан",
@@ -43,3 +24,23 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll(".promo__adv img");
+const genre = document.querySelector(".promo__genre");
+const poster = document.querySelector(".promo__bg");
+const movieList = document.querySelector(".promo__interactive-item");
+
+adv.forEach(item => item.remove());
+genre.textContent = "Драма";
+poster.style.backgroundImage = "url(img/bg.jpg)";
+
+movieDB.movies.sort().forEach((movie, i) => {
+    movieList.innerHTML += `
+       <li class="promo__interactive-item">
+            ${i + 1}
+            ${movie}
+            <div class="delete"></div>
+        </li>
+        `
+    });
+
+console.log(movieDB)
